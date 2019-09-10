a25ebd2cf8ae3f5e5598b8129ef0f9f3d5de74f4
Cornelius Suermann <cornelius.suermann@camunda.com>
2019-09-10 16:36:32 +0200

    add TLS and oAuth support

--- nodes/zeebe.js
+++ nodes/zeebe.js
@@ -1,18 +1,35 @@
 module.exports = function(RED) {
     const ZB = require('zeebe-node');
-    
-    function Zeebe(n) {      
-        RED.nodes.createNode(this, n);
-        this.host = n.host;
-        this.port = n.port;
-        this.zbc = new ZB.ZBClient(`${this.host}:${this.port}`);
 
-        this.on('close', function(done) {
-            return this.zbc.close().then(() => {
-                console.log("All workers closed");
+    function Zeebe(config) {
+        RED.nodes.createNode(this, config);
+
+        const node = this;
+
+        const options = {
+            useTls: Boolean(config.useTls),
+            oAuth: {
+                url: config.oAuthUrl,
+                audience: config.contactPoint.split(':')[0],
+                clientId: config.clientId,
+                clientSecret: config.clientSecret,
+                cacheOnDisk: false,
+            },
+        };
+
+        if (config.oAuthUrl === '') {
+            delete options.oAuth;
+        }
+
+        node.zbc = new ZB.ZBClient(config.contactPoint, options);
+
+        node.on('close', function(done) {
+            return node.zbc.close().then(() => {
+                console.log('All workers closed');
                 done();
-            })
+            });
         });
     }
-    RED.nodes.registerType("zeebe", Zeebe);
-}
+
+    RED.nodes.registerType('zeebe', Zeebe);
+};
