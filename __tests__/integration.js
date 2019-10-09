6f62521ef59763d0fba1eb9d85ad9000f1415675
pedesen <patrick.dehn@camunda.com>
2019-10-09 11:54:20 +0200

    remove zbctl status from travis config

--- __tests__/integration.js
+++ __tests__/integration.js
@@ -237,7 +237,7 @@ describe('integration', () => {
                                     correlationKey: processId,
                                 },
                             });
-                        }, 500);
+                        }, 1000);
                     });
                 });
 
